9a7ce8e67dcccd6c93260d6d710be5f6eb6cbebe
Artyom Grigoriev <ortemij@gmail.com>
2011-06-02 15:56:18 +0000

    compile.sh with Closure Compiler
    You need to download it from http://closure-compiler.googlecode.com/files/compiler-latest.zip and to place compiler.jar into project root.
    
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@64 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages/5/sources/compile.sh
+++ messages/5/sources/compile.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-cat auxiliary.js sys.js ui.js stat-counter.js message-processor.js api-connector.js start.js > ../bookmarklet/message-stats-core.js
+cat auxiliary.js > ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat sys.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat ui.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat stat-counter.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat message-processor.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat api-connector.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+cat start.js >> ../bookmarklet/message-stats-core-src.js
+echo >> ../bookmarklet/message-stats-core-src.js
+
+java -jar ../../../compiler.jar --js ../bookmarklet/message-stats-core-src.js --js_output_file ../bookmarklet/message-stats-core.js
