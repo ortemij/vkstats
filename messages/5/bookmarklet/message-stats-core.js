d1b143ab324ec4033cd13ba6a1be11c441a87b4c
Artyom Grigoriev <ortemij@gmail.com>
2011-06-02 15:17:43 +0000

    syntax error fix
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@63 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages/5/bookmarklet/message-stats-core.js
+++ messages/5/bookmarklet/message-stats-core.js
@@ -437,7 +437,7 @@ var user = {
 		var cPane = ce('div', {id:"mail_bar", className: 'clear_fix bar bar clearFix actionBar', innerHTML:
 			'<div id="vkstats_text"><span>' + user.lang.thankYou + '</span>' + (messageProcessor.failed > 0 ? ' ' + user.lang.warning + ': ' + messageProcessor.failed : '') + '<div style="float:right"> &copy; <a href="' + SYS.LINK_TO_CLUB + '" target="_blank">vkontakte-stats</a>, 2010 &ndash; 2011</div></div>'
 		});
-		var mActions = ce('div', {id: "mail_bar_act", innerHTML: '<span class="fl_l" style="padding-top:4px;">' + user.lang.withSelected + ': </span>',class:"fl_l"}, {display: 'none', paddingTop:"5px"});
+		var mActions = ce('div', {id: "mail_bar_act", innerHTML: '<span class="fl_l" style="padding-top:4px;">' + user.lang.withSelected + ': </span>',"class":"fl_l"}, {display: 'none', paddingTop:"5px"});
 		/*
 		mActions.innerHTML += '<span id="mail_summary" style="display:none;"></span><a href="#" onclick="statCounter.exportToNote();">' + user.lang.exportToNote + '</a>';
 		if(user.plotGraphs) {
