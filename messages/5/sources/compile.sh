7fc60d106cb32655c2c42beef55f7ff983cb50c7
Artyom Grigoriev <ortemij@gmail.com>
2011-06-04 14:55:58 +0000

    auto compilation with Google Closure!
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@68 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages/5/sources/compile.sh
+++ messages/5/sources/compile.sh
@@ -21,4 +21,8 @@ echo >> ../bookmarklet/message-stats-core-src.js
 cat start.js >> ../bookmarklet/message-stats-core-src.js
 echo >> ../bookmarklet/message-stats-core-src.js
 
-java -jar ../../../compiler.jar --js ../bookmarklet/message-stats-core-src.js --js_output_file ../bookmarklet/message-stats-core.js
+native2ascii -encoding windows-1251 ../bookmarklet/message-stats-core-src.js ../bookmarklet/message-stats-core-ascii.js
+java -jar ../../../compiler.jar --js ../bookmarklet/message-stats-core-ascii.js --js_output_file ../bookmarklet/message-stats-core.js --compilation_level SIMPLE_OPTIMIZATIONS
+
+rm ../bookmarklet/message-stats-core-src.js
+rm ../bookmarklet/message-stats-core-ascii.js
