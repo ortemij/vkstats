a23b40b7efd995ea93bd73bd6425d89b758dc682
Artyom Grigoriev <ortemij@gmail.com>
2011-06-05 09:34:36 +0000

    New super-puper compilator! Using: 1) ./compile.sh for simple build; 2) ./compile.sh version [comment] for commiting new version in builds folder
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@78 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages/5/sources/compile.sh
+++ messages/5/sources/compile.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ -n "$1" ]; then
+  VERSION="$1"
+  SUFFIX="-$VERSION"
+  svn up ../../..
+  if [ -e ../builds/message-stats-core$SUFFIX.js ]; then
+    echo "Version $VERSION already exists!"
+    exit 1
+  fi
+else
+  VERSION=""
+  SUFFIX=""
+fi
+
 cat auxiliary.js > ../bookmarklet/message-stats-core-src.js
 echo >> ../bookmarklet/message-stats-core-src.js
 
@@ -18,11 +31,26 @@ echo >> ../bookmarklet/message-stats-core-src.js
 cat api-connector.js >> ../bookmarklet/message-stats-core-src.js
 echo >> ../bookmarklet/message-stats-core-src.js
 
+if [ -n "$VERSION" ]; then
+  echo >> ../bookmarklet/message-stats-core-src.js
+  echo "SYS.VERSION='$VERSION';" >> ../bookmarklet/message-stats-core-src.js
+  echo >> ../bookmarklet/message-stats-core-src.js
+fi
+
 cat start.js >> ../bookmarklet/message-stats-core-src.js
 echo >> ../bookmarklet/message-stats-core-src.js
 
 native2ascii -encoding windows-1251 ../bookmarklet/message-stats-core-src.js ../bookmarklet/message-stats-core-ascii.js
 java -jar ../../../compiler.jar --js ../bookmarklet/message-stats-core-ascii.js --js_output_file ../bookmarklet/message-stats-core.js --compilation_level SIMPLE_OPTIMIZATIONS
 
+if [ -n "$VERSION" ]; then
+  cp ../bookmarklet/message-stats-core.js ../builds/message-stats-core$SUFFIX.js
+fi
+
 rm ../bookmarklet/message-stats-core-src.js
 rm ../bookmarklet/message-stats-core-ascii.js
+
+if [ -n "$VERSION" ]; then
+  svn add ../builds/message-stats-core$SUFFIX.js
+  svn commit ../builds/message-stats-core$SUFFIX.js ../bookmarklet/message-stats-core.js -m "$VERSION: $2"
+fi
