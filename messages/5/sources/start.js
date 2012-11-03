84791b306d6b914c5c4bc25fc888dfd5b1ff4442
Mikhail Raevskiy <KiberInfinity@gmail.com>
2012-11-03 17:16:01 +0000

    При наличии установленного VkOpt'а будет использоваться его модуль взаимодействия с API
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@90 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages/5/sources/start.js
+++ messages/5/sources/start.js
@@ -1,9 +1,15 @@
 if (window.renderFlash){
 	stManager.add(SYS.FILES_TO_LOAD, function() {
-		vk_api.Auth(function(mid,secret,sid){
-			user.uid=mid;
-			ui.requestSettings();
-		});	
+		if (window.dApi){ /*Use VkOpt API Module */
+			vk_api=dApi;
+         user.uid=vk.id;
+			ui.requestSettings();      
+      } else {
+         vk_api.Auth(function(mid,secret,sid){
+            user.uid=mid;
+            ui.requestSettings();
+         });	
+      }
 	});
 } else {
 	alert(user.lang.wrongPage);
