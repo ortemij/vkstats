8c69cf60411af404e43d1070b828eda31f3b5849
Artyom Grigoriev <ortemij@gmail.com>
2013-03-30 19:07:42 +0000

    Issue 6: New emblems
    git-svn-id: http://vkontakte-stats.googlecode.com/svn/trunk@97 ab54aacd-911c-58a9-bec3-9916de6c1d4d

--- messages-chrome/inline.js
+++ messages-chrome/inline.js
@@ -1,18 +1,42 @@
 (function () {
-	var divider = document.createElement("span");
-	divider.className = "divider";
-	divider.innerHTML = "|";
-	var span = document.createElement("span");
-	var href = document.createElement("a");
-	href.onclick = function () {
-		var a = document.createElement('script'); 
-		a.type='text/javascript'; 
-		a.src=location.protocol+'//vkontakte-stats.googlecode.com/files/message-stats-core-4.3.4.js?' + Math.round((new Date).getTime()/60); 
-		document.getElementsByTagName('head')[0].appendChild(a)
-	};
-	href.innerHTML = "Статистика сообщений";
-	span.appendChild(href);
+	var attach = function () {
+		var divider = document.createElement("span");
+		divider.className = "divider";
+		divider.innerHTML = "|";
+		var span = document.createElement("span");
+		var href = document.createElement("a");
+		href.onclick = function () {
+			var a = document.createElement('script'); 
+			a.type='text/javascript'; 
+			a.src=location.protocol+'//vkontakte-stats.googlecode.com/files/message-stats-core-4.3.4.js?' + Math.round((new Date).getTime()/60); 
+			document.getElementsByTagName('head')[0].appendChild(a)
+		};
+		href.innerHTML = "Статистика сообщений";
+		span.appendChild(href);
 
-	document.getElementById('im_dialogs_summary').children[0].appendChild(divider);
-	document.getElementById('im_dialogs_summary').children[0].appendChild(span);
+		document.getElementById('im_dialogs_summary').children[0].appendChild(divider);
+		document.getElementById('im_dialogs_summary').children[0].appendChild(span);
+	};
+
+	var onLocationChange = function () {
+		attach();
+	};
+
+	var lastLocation = window.location.href;
+	var regex = /.*\/im$/;
+
+	if (regex.exec(lastLocation)) {
+		attach();
+	}
+
+	window.setInterval(function () {
+		var l = window.location.href;
+		if (lastLocation != l) {
+			var match = regex.exec(l);
+			if (match) {
+				onLocationChange();
+			}
+		}
+		lastLocation = l;
+	}, 100);
 })();
